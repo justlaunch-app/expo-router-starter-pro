b1a73e0f8b84ad6e9f0c5f0637a4031a6118b2b1
Zoltan Fodor <me@zoltanfodor.dev>
2024-03-03 21:12:58 +0100

    CHORE: add comment how to use Vexo

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
+/**
+ * Vexo is not supported on Expo Go. If you want to use Vexo, you need to build a standalone app with EAS.
+ * For analytics with Vexo you need to add the following code:
+ *
+ * import { vexo } from 'vexo-analytics';
+ * vexo('YOUR_API_KEY');
+ */
+
 import 'expo-router/entry';
