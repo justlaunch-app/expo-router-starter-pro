31b274c33a3b6e8fc4e025367c1faac83b6d4336
Zoltan Fodor <me@zoltanfodor.dev>
2024-03-11 22:28:34 +0100

    FIX: update Material Top tabs style

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,13 +1,20 @@
 import SafeAreaView from '@components/core/SafeAreaView/SafeAreaView';
 import { MaterialTopTabs } from '@layouts/material-top-tabs';
 import useHeaderVisibility from '@hooks/useHeaderVisibility';
+import { SCREEN_WIDTH } from '@utils/deviceSize';
 
 export default function IndexTopTabsLayout() {
   useHeaderVisibility(false);
 
   return (
     <SafeAreaView>
-      <MaterialTopTabs />
+      <MaterialTopTabs
+        screenOptions={{
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+          tabBarItemStyle: { width: SCREEN_WIDTH / 2 },
+        }}
+      />
     </SafeAreaView>
   );
 }
--- src/app/(root)/(tabs)/(index)/_layout.tsx
+++ src/app/(root)/(tabs)/(index)/_layout.tsx
@@ -1,5 +1,14 @@
 import { MaterialTopTabs } from '@layouts/material-top-tabs';
+import { SCREEN_WIDTH } from '@utils/deviceSize';
 
 export default function IndexTopTabsLayout() {
-  return <MaterialTopTabs />;
+  return (
+    <MaterialTopTabs
+      screenOptions={{
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+        tabBarItemStyle: { width: SCREEN_WIDTH / 2 },
+      }}
+    />
+  );
 }
