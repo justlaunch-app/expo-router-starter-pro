65692d3a4116e31247bc5628ea29e59b512994cc
Zoltan Fodor <me@zoltanfodor.dev>
2024-03-24 11:36:40 +0100

    CHORE: add comment to Lucide Icon

--- src/components/core/Icon/LucideIcon.tsx
+++ src/components/core/Icon/LucideIcon.tsx
@@ -1,3 +1,25 @@
+/**
+ * LucideIcon component
+ * @description
+ * A component that renders a Lucide icon.
+ * @module Icon
+ * @param {string} name - The name of the icon to render.
+ * @param {string} color - The color of the icon.
+ * @param {number} size - The size of the icon.
+ * @return {React.FC<LucideIconProps>}
+ * @example
+ * import Icon from '@components/core/Icon/LucideIcon';
+ *
+ * const MyComponent = () => {
+ *  return <Icon name="activity" color="black" size={24} />;
+ * };
+ *
+ * export default MyComponent;
+ * @see
+ *
+ * - [Lucide](https://lucide.dev/)
+ */
+
 import React from 'react';
 import { icons, LucideProps } from 'lucide-react-native';
 import { ColorValue } from 'react-native';
